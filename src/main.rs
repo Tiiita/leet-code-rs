29473e96b58e1329d633c7b1f864cb7149dc92dc
tiiita <tiiitamail@gmail.com>
2024-09-17 13:41:10 +0200

    Seperate tasks in files

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,5 @@
+mod palindrome;
 fn main() {
-    palindrome(12321); //Should print yes
-    palindrome(1234); //Should print 
-    palindrome(19191); //Should print yes again
+    palindrome::test();
 }
 
-fn palindrome(num: i32) {
-    let binding = num.to_string();
-    let as_chars: Vec<char> = binding.chars().into_iter().collect();
-
-    for (i, ele) in  as_chars.iter().enumerate() {
-        if let Some(char_end) = as_chars.get(as_chars.len() - i - 1) {
-            if ele.eq_ignore_ascii_case(char_end) {
-                continue;
-            } else {
-                println!("No palindrome!");
-                return;
-            }
-        }
-    }
-
-    println!("Is a palindrome!");
-}
